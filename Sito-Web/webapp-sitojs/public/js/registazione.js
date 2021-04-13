6090616338a0c118c46c883891f16a997aecb517
Daniele Capra <58602976+Danyelovich@users.noreply.github.com>
2021-04-13 11:03:53 +0200

    Add files via upload

---
+++ Sito-Web/webapp-sitojs/public/js/registazione.js
@@ -0,0 +1,21 @@
+function controllaRegistrazione() {
+    let mioForm = document.getElementsByClassName("myform");
+    let flagCampiPieni = true;
+    let tagInput = document.getElementsByTagName("input");
+    for (let i = 0; i < (tagInput.length); i++) {
+        if (tagInput[i].value == "") {
+            flagCampiPieni = false;
+            tagInput[i].style.border = " solid red 2px";
+            //tagInput[i].style.color = "red ";
+        } else {
+            tagInput[i].style.border = " solid black 2px";
+            //tagInput[i].style.color = "black ";
+        }
+    }
+    if (tagInput[6].value != tagInput[7].value) {
+        flagCampiPieni = false;
+    }
+    if (flagCampiPieni == true) {
+        mioForm.submit();
+    } else {}
+}
